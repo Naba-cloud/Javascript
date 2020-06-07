df8e6262b36759224ac90bb4fb454c5db9735a1a
Naba-cloud <59452017+Naba-cloud@users.noreply.github.com>
2020-06-07 04:46:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+//Assignment 1
+alert("Hello Welcome to my page");
+alert("Error!please enter a valid password");
+alert("Welcome to JS land...... \n Happy Coding! ");
+alert("Welcome To Js land");
+alert("Prevent this page from creating additional dialogues");
+alert("Hello....... I can run js through my web browser console");
+//Assignment 2
+var username;
+var myName="Naba Zehra";
+var message;
+message="Hello World";
+alert(message);
+//Assignment 3
+var name="Naba";
+alert(name);
+var age="20";
+alert(age);
+var certificate="Certified Mobile Application Development";
+alert(certificate);
+var food="Pizza";
+alert(food);
+alert(food.slice(0,4));
+alert(food.slice(0,3));
+alert(food.slice(0,2));
+alert(food.slice(0,1));
+var email="someone@gmail.com"
+
+alert("My email address is "+email);
+var book="A smarter way to learn JavaScript";
+alert("I am trying to learn from the book "+ book);
+document.write("Yah! I can write Html content through Javascript");
+var store="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(store);
+//Assignment 3
+var age;
+age="21";
+alert(age);
+var visitortrack=14;
+alert("You have visited this site "+visitortrack + " times");
+var birthYear=1990;
+document.write("My Birth Year is "+birthYear, "\r" ," \r Data type of my declared variable is" + typeof(birthYear));
+var Visitors_name, Product_title,Quantity;
+Visitors_name="John Doe".bold();
+Product_title="T- shirts".bold();
+Quantity=" 5 ".bold();
+document.write(Visitors_name+" ordered "+ Quantity + Product_title +"(s)".bold()+"on XYZ Clothing store");
+//Assignment 5
+var x = prompt("Enter a Value");
+var y = prompt("Enter a Value");
+var num1 = parseInt(x);
+var num2 = parseInt(y);
+var sum=num1+num2;
+document.write("The sum of ","\t",num1 ," and \t", num2,"is \t"+sum);
+//task2 of assignment 5
+var differnce=num1-num2;
+document.write("\n \n The differnce of ","\t",num1 ," and \t", num2,"is \t"+differnce);
+
